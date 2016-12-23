965a4424c6edb076d7ca15b01157f389a5a2ccdf
jonnykry93@gmail.com <jonnykry93@gmail.com>
2016-12-23 17:54:27 -0600

    init commit after using create-react generator

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Expresso</h1>
+        </div>
+        <p className="App-intro">
+          Welcome to the Expresso web application!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
