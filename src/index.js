67ae02cea9169a026a61ffb2928db1b61b9647d8
jonnykry93@gmail.com <jonnykry93@gmail.com>
2017-01-21 16:06:08 -0600

    add the about page to route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import About from './components/About';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Register from './components/Register';
@@ -31,6 +32,7 @@ ReactDOM.render(
     <Provider store={store}>
         <Router history={browserHistory}>
             <Route path="/" component={App} />
+            <Route path="/About" component={About} />
             <Route path="/login" component={Login} />
             <Route path="/logout" component={Logout} />
             <Route path="/register" component={Register} />
