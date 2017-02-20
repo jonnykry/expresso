fce82b4ce5339d0c8088bd94eb0c483658d421a8
jonnykry93@gmail.com <jonnykry93@gmail.com>
2017-02-20 16:25:18 -0600

    clean up payment reducer

--- src/reducers/coinageReducer.js
+++ src/reducers/coinageReducer.js
@@ -8,39 +8,21 @@ import {
     ERROR_UPDATING_CUSTOMER_PAYMENT_INFO
 } from '../actions/coinageActions'
 
-export function createCustomerPaymentInfo(state = {
+export function customerPaymentInfoReducer(state = {
     isFetching: false
 }, action) {
     switch (action.type) {
+        case REQUEST_UPDATE_CUSTOMER_PAYMENT_INFO:
         case REQUEST_CREATE_CUSTOMER_PAYMENT_INFO:
             return Object.assign({}, state, {
                 isFetching: true
             });
+        case RECEIVE_UPDATED_CUSTOMER_PAYMENT_INFO:
         case RECEIVE_CREATED_CUSTOMER_PAYMENT_INFO:
             return Object.assign({}, state, {
                 isFetching: false
             });
         case ERROR_CREATING_CUSTOMER_PAYMENT_INFO:
-            return Object.assign({}, state, {
-                isFetching: false
-            });
-        default:
-            return state
-    }
-}
-
-export function updateCustomerPaymentInfo(state = {
-    isFetching: false
-}, action) {
-    switch (action.type) {
-        case REQUEST_UPDATE_CUSTOMER_PAYMENT_INFO:
-            return Object.assign({}, state, {
-                isFetching: true
-            });
-        case RECEIVE_UPDATED_CUSTOMER_PAYMENT_INFO:
-            return Object.assign({}, state, {
-                isFetching: false
-            });
         case ERROR_UPDATING_CUSTOMER_PAYMENT_INFO:
             return Object.assign({}, state, {
                 isFetching: false
@@ -49,4 +31,3 @@ export function updateCustomerPaymentInfo(state = {
             return state
     }
 }
-
--- src/reducers/configureReducers.js
+++ src/reducers/configureReducers.js
@@ -1,29 +1,20 @@
 import { combineReducers } from 'redux';
-<<<<<<< e178cf68f278c56ed58b8cc3897f6116ddd2ad80
-import { authenticateUser, createUser } from './userReducer';
-import { createRoaster } from './roasterReducer';
-=======
+
+import { warehouseReducer } from './warehouseReducer';
+import { roasterReducer } from './roasterReducer';
 import { userReducer } from './userReducer';
->>>>>>> change user exposure on front-end and update auth
-import { createCustomerPaymentInfo, updateCustomerPaymentInfo } from './coinageReducer';
-import {
-	getAllContent, createContent, deleteContent,
-	getAllTriggers, createTrigger, deleteTrigger
-} from './bloodlinesReducer';
+import { customerPaymentInfoReducer } from './coinageReducer';
+import { triggers, contents, receipts, modify } from './bloodlinesReducer';
 
 const rootReducer = combineReducers({
+    roasterReducer,
     userReducer,
-    createCustomerPaymentInfo,
-    updateCustomerPaymentInfo,
-    getAllContent,
-    createContent,
-    deleteContent,
-    getAllTriggers,
-    createTrigger,
-    deleteTrigger,
-    authenticateUser,
-    createUser,
-    createRoaster
+    customerPaymentInfoReducer,
+    warehouseReducer,
+    triggers,
+    contents,
+    receipts,
+    modify
 });
 
 export default rootReducer;
