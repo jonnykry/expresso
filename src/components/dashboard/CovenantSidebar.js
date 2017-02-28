06c29f7af0361fbc225c35a7eaaf322505dc0367
Derek Yu <yuderekyu@gmail.com>
2017-02-28 17:48:49 -0600

    Add covenant sidebar

--- src/components/dashboard/CovenantSidebar.js
+++ src/components/dashboard/CovenantSidebar.js
@@ -1,11 +1,27 @@
-import React { Component } from 'react';
-iport SidebarSelector from './SidebarSelector';
+import React, { Component } from 'react';
+import SidebarSelector from './SidebarSelector';
 
 import { connect } from 'react-redux';
 
 class CovenantSidebar extends Component {
 	render() {
-		const d = "/dashboard/subscription";
+		const b = this.props.location;
+		const d = "/dashboard/subscription";	
 
+		const content = this.props.items === ''? //what is going on
+			<SidebarSelector subSelector={true} name="View all subscriptions" to={d + "subscriptions"} location={b} />;
+		return (
+			<div>
+				{content}
+			</div>
+		)
 	}
 }
+
+function mapStateToProps(state){
+	return{
+		items: state.subscriptionReducer.items
+	};
+}
+
+export default connect(mapStateToProps)(CovenantSidebar);
--- src/components/dashboard/SidebarContent.js
+++ src/components/dashboard/SidebarContent.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import SidebarSelector from './SidebarSelector';
 import BloodlinesSidebar from './BloodlinesSidebar';
 import RoasterSidebar from './RoasterSidebar';
+import CovenantSidebar from './CovenantSidebar';
 
 class SidebarContent extends Component {
     render() {
         const bloodlines = <BloodlinesSidebar location={this.props.location} />;
         const roaster = <RoasterSidebar location={this.props.location} />;
+        const covenant = <CovenantSidebar location={this.props.location} />;
 
         const b = this.props.location;
         const d = "/dashboard/";
@@ -17,6 +19,8 @@ class SidebarContent extends Component {
                                      children={null} location={b} />
                     <SidebarSelector name="Bloodlines" to={d+"bloodlines"}
                         children={bloodlines} location={b} />
+                    <SidebarSelector name="Covenant" to={d+"covenant"}
+                        children={covenant} location={b} />
                     <SidebarSelector name="Roaster" to={d+"roaster"}
                                      children={roaster} location={b} />
                     <SidebarSelector name="Account Settings" to={d+"settings"}
