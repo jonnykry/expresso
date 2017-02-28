ddcf479b6a6902380ae4b87a43dcd5b5efe25434
garret.meier <garret.meier@bunchball.com>
2017-02-28 15:39:37 -0600

    Linting

--- src/components/dashboard/BloodlinesSidebar.js
+++ src/components/dashboard/BloodlinesSidebar.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component, PropTypes} from 'react';
 import SidebarSelector from './SidebarSelector';
 
 class BloodlinesSidebar extends Component {
-
-	render() {
-		const b = this.props.location;
-		const d = "/dashboard/bloodlines/";
-		return (
-			<div>
-				<SidebarSelector subSelector={true} name="Content" to={d+"content"} location={b} />
-				<SidebarSelector subSelector={true} name="Triggers" to={d+"trigger"} location={b} />
-				<SidebarSelector subSelector={true} name="Receipts" to={d+"receipt"} location={b} />
-				<SidebarSelector subSelector={true} name="Preferences" to={d+"preference"} location={b} />
-			</div>
-		)
-	}
+    render() {
+        const b = this.props.location;
+        const d = '/dashboard/bloodlines/';
+        return (
+            <div>
+                <SidebarSelector subSelector name={'Content'} to={d + 'content'} location={b}/>
+                <SidebarSelector subSelector name={'Triggers'} to={d + 'trigger'} location={b}/>
+                <SidebarSelector subSelector name={'Receipts'} to={d + 'receipt'} location={b}/>
+                <SidebarSelector subSelector name={'Preferences'} to={d + 'preference'} location={b}/>
+            </div>
+        );
+    }
 }
+
+BloodlinesSidebar.propTypes = {
+    location: PropTypes.string.isRequired
+};
 
 export default BloodlinesSidebar;
--- src/components/dashboard/SidebarSelector.js
+++ src/components/dashboard/SidebarSelector.js
@@ -1,52 +1,60 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
 
 class SidebarSelector extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state ={
-		    active: false
-		};
-	}
+        this.state = {
+            active: false
+        };
+    }
 
-	hasChildren(active) {
-		if (active) {
-			return (<div className="b--black">{ this.props.children }</div>);
-		} else {
-			return (<div></div>);
-		}
-	}
-
-	render() {
-		let linkClass = 'no-underline black';
-		let btnClass = 'pointer f6 pa1 center w-100 link dim black pl3 pv3';
-
-		const active = this.props.location.includes(this.props.to);
-		const dropdown = this.hasChildren(active);
-
-		if (this.props.subSelector) {
-			btnClass += ' pl4';
-			linkClass += ' black';
-		}
-
-		if (active) {
-		    btnClass += ' b';
+    hasChildren(active) {
+        if (active) {
+            return (<div className="b--black">{ this.props.children }</div>);
         }
 
-		return (
-			<div>
-			<Link className={linkClass} to={this.props.to}>
-				<div className={btnClass}>
-					{this.props.name}
-	            </div>
+        return (<div/>);
+    }
 
-            </Link>
-            	{dropdown}
+    render() {
+        let linkClass = 'no-underline black';
+        let btnClass = 'pointer f6 pa1 center w-100 link dim black pl3 pv3';
+
+        const active = this.props.location.includes(this.props.to);
+        const dropdown = this.hasChildren(active);
+
+        if (this.props.subSelector) {
+            btnClass += ' pl4';
+            linkClass += ' black';
+        }
+
+        if (active) {
+            btnClass += ' b';
+        }
+
+        return (
+            <div>
+                <Link className={linkClass} to={this.props.to}>
+                    <div className={btnClass}>
+                        {this.props.name}
+                    </div>
+
+                </Link>
+                {dropdown}
             </div>
-		);
-	}
+        );
+    }
 
 }
+
+SidebarSelector.propTypes = {
+    name: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    subSelector: PropTypes.bool,
+    children: PropTypes.object
+};
 
 export default SidebarSelector;
