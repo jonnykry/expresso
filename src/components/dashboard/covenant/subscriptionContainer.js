48ad4faa6222cd96f60f52e40774715d6c58b9f1
Derek Yu <yuderekyu@gmail.com>
2017-02-27 21:39:33 -0600

    Add more logic for subscription actions and reducer

--- src/components/dashboard/covenant/subscriptionContainer.js
+++ src/components/dashboard/covenant/subscriptionContainer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 //import actions
+import{ getSubscriptionsByUser} from '../../../actions/covenantActions';
 
 class SubscriptionContainer extends Component() {
 	constructor(props) {
 		super(props);
-
-
+		dispatch(getSubscriptionsByUser(props.id, offset, 20)).then(this.nextPage.bind(this))
 	}
 	/*Dispatch the action before rendering*/
 	componentDidMount(){
@@ -14,14 +14,18 @@ class SubscriptionContainer extends Component() {
 	}
 	render () {
 		return (
-			<div> </div>
+			<div> 
+				<SubscriptionList {...this.props.items}/>
+			</div>
 			);
 	}
 }
 
 
 function mapStateToProps(state){
-
+	return{
+		items: state.subscriptions
+	}
 }
 
-export default connect()(SubscriptionContainer);
+export default connect(mapStateToProps)(SubscriptionContainer);
--- src/reducers/covenantReducer.js
+++ src/reducers/covenantReducer.js
@@ -1,13 +1,9 @@
 import ActionTypes from '../actions/actionTypes';
-import {handlePagedAction, handleModifyAction} from './bloodlinesReducer'
+import {getPagedState, handlePagedAction, handleModifyAction} from './bloodlinesReducer'
 
-export function subscription(state = {
-	error: false,
-	success: false,
-	subscription:'',
-}, action) {
-	if (action.type !== Actiontypes.Subscription) {
-		reuturn state;
+export function subscription(state = getPagedState(), action) {
+	if (action.type !== Actiontypes.SUBSCRIPTIONS) {
+		return state;
 	}
 	return handlePagedAction(action, state);
 }
