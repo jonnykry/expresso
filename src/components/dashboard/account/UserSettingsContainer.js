881e8bdba6efce1284543be46ea75c0e8314a16c
Jake Long <jakeprogrammer@gmail.com>
2017-03-08 23:14:21 -0600

    Fixed user update

--- src/components/dashboard/account/UserSettingsContainer.js
+++ src/components/dashboard/account/UserSettingsContainer.js
@@ -5,30 +5,35 @@ import { updateUserInfo } from '../../../actions/userActions'
 import AccountInfo from './AccountInfo';
 
 class UserSettingsContainer extends Component {
-    onHandleSubmit(e) {
-        e.preventDefault();
+    constructor(props) {
+        super(props);
 
+        this.updateUserBind = this.onHandleSubmit.bind(this);
+    }
+
+    onHandleSubmit(refs) {
         const { dispatch } = this.props;
-				const { firstName, lastName, phone, email, addressLine1, addressLine2, city, state, zipCode, country, password, confirmPassword } = this.refs;
+		const { firstName, lastName, phone, email, addressLine1, addressLine2, city, state, zipCode, country, password, confirmPassword } = refs;
 
         if(password.value !== '' && password.value !== confirmPassword.value) {
           //TODO: Error
         } else {
           const userInfo = {
+            id: this.props.user.id,
           	firstName: firstName.value,
-  					lastName: lastName.value,
-  					phone: phone.value,
-  					email: email.value,
-  					addressLine1: addressLine1.value,
-  					addressLine2: addressLine2.value,
-  					addressCity: city.value,
-  					addressState: state.value,
-  					addressZip: zipCode.value,
-  					addressCountry: country.value,
+  			lastName: lastName.value,
+  			phone: phone.value,
+  			email: email.value,
+  			addressLine1: addressLine1.value,
+  			addressLine2: addressLine2.value,
+  			addressCity: city.value,
+  			addressState: state.value,
+  			addressZip: zipCode.value,
+  			addressCountry: country.value,
             passHash: password.value
           };
 
-  			  dispatch(updateUserInfo(userInfo, this.props.user.id));
+  		  dispatch(updateUserInfo(userInfo, this.props.user.id));
         }
     }
 
@@ -48,7 +53,7 @@ class UserSettingsContainer extends Component {
 
 function mapStateToProps(state) {
 	return {
-    user: state.userReducer.user,
+        user: state.userReducer.user,
 	};
 }
 
