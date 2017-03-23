d42bbfd88637cc0821a71041e46d13093ff40c39
Jake Long <jakeprogrammer@gmail.com>
2017-03-23 15:56:54 -0500

    Roaster is now retrieved in the dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 import { getUserInfo } from '../../actions/userActions';
+import { getRoaster } from '../../actions/roasterActions';
 
 import SidebarContent from './SidebarContent';
 
@@ -9,16 +10,16 @@ import './Dashboard.css';
 
 class Dashboard extends Component {
     componentDidMount() {
-        console.log(this.props.user);
         if(this.props.user === null) {
-            console.log('here');
             this.props.dispatch(getUserInfo(localStorage.getItem('userId')));
+        }
+        if(this.props.roaster === null) {
+            this.props.dispatch(getRoaster(localStorage.getItem('roasterId')));
         }
     }
 
     render() {
         let sidebar = <SidebarContent location={this.props.location.pathname} />;
-        console.log(this.props.user);
         return (
             <div className="w-100 min-h-100 h-100 absolute dib">
                 <div className="h-100 dashboard">
@@ -36,7 +37,8 @@ class Dashboard extends Component {
 
 function mapStateToProps(state) {
 	return {
-        user: state.userReducer.user
+        user: state.userReducer.user,
+        roaster: state.roaster.roaster
 	};
 }
 
--- src/components/dashboard/roaster/RoasterAccount.js
+++ src/components/dashboard/roaster/RoasterAccount.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updateRoaster } from '../../../actions/roasterActions';
+import { updateRoaster, getRoaster } from '../../../actions/roasterActions';
 
 import AccountInfo from '../../AccountInfo';
 
