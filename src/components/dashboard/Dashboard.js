6c6dbaaca76ba6620dd69c7ec5303b280d42133d
Jake Long <jakeprogrammer@gmail.com>
2017-03-23 15:00:47 -0500

    On refresh user information is retreived

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+import { getUserInfo } from '../../actions/userActions';
 
 import SidebarContent from './SidebarContent';
 
 import './Dashboard.css';
 
 class Dashboard extends Component {
+    componentDidMount() {
+        console.log(this.props.user);
+        if(this.props.user === null) {
+            console.log('here');
+            this.props.dispatch(getUserInfo(localStorage.getItem('userId')));
+        }
+    }
+
     render() {
         let sidebar = <SidebarContent location={this.props.location.pathname} />;
-
+        console.log(this.props.user);
         return (
             <div className="w-100 min-h-100 h-100 absolute dib">
                 <div className="h-100 dashboard">
@@ -23,4 +34,10 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+function mapStateToProps(state) {
+	return {
+        user: state.userReducer.user
+	};
+}
+
+export default connect(mapStateToProps)(Dashboard);
