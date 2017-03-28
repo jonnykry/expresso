ceeaa5510300159d4f77e3e1ed37e87f5a9b6b00
garret.meier <garret.meier@bunchball.com>
2017-03-28 11:52:15 -0500

    Added images and expresso svg

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,13 +10,15 @@ class Navigation extends Component {
             <Link to="/logout" className={linkClass} title="Logout">Logout</Link> :
             <Link to="/login" className={linkClass} title="Login">Login</Link>;
         const dashOrRegister = isAuthed ?
-            <Link to="/dashboard" className={dashClass} title="Dashboard">Dashboard</Link> : 
+            <Link to="/dashboard" className={dashClass} title="Dashboard">Dashboard</Link> :
             <Link to="/register" className={dashClass} title="Register">Sign up Free</Link>;
 
         return (
             <nav className="flex justify-between bg-blue shadow-2">
-                    <Link to="/" className="f1-l b i link dim white flex items-center pv3 ph5" title="Home">Expresso</Link>
-                <div className="flex-grow pa3 pv5 flex items-center">
+                    <Link to="/" className="link flex items-center pv4 ph3" title="Home">
+                        <object type="image/svg+xml" data="/title_white.svg" className="w-70"></object>
+                    </Link>
+                <div className="flex-grow pa3 pv4 flex items-center">
                     <Link to="/" className={linkClass} title="Home">Home</Link>
                     <Link to="/about" className={linkClass} title="About">About</Link>
                     {logInOrLogOut}
