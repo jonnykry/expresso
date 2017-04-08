5aa22f2b10d35fbdc4a9b84a7c7a6d060cf248e0
jonnykry93@gmail.com <jonnykry93@gmail.com>
2017-04-08 17:53:30 -0500

    put started date on sub

--- src/components/dashboard/subscriptions/Subscription.js
+++ src/components/dashboard/subscriptions/Subscription.js
@@ -15,18 +15,23 @@ class Subscription extends Component {
 		const btnClass = 'pointer dim br1 ba bw1 tc pa2 black';
 		const statusLabel = item.status === 'ACTIVE' ? 'Deactivate' : 'Activate';
 
+		const date = new Date(item.createdAt);
+
 		return (
-			<div className="mw7 flex center bl br bt bb mb2 pa3">
-				<Link className="pa1 mr2 black no-underline ba bw1" to={'/dashboard/browse/' + item.itemId}>View Item Details</Link>
-				<div className={btnClass + ' w4 pr3 mr3'} onClick={() => this.props.onStatusUpdate(item)}>{statusLabel}</div>
-				<div className="w-25 pt2"><strong># Bags:</strong> {item.quantity}</div>
-				<div className="b pv2 pr2">Frequency:</div>
-				<Select className="w-50 mr3 h-100"
-						options={frequencyOptions}
-						simpleValue
-						clearable={false}
-						value={item.frequency}
-						onChange={(val) => this.props.onFrequencyChange(item, val)} />
+			<div className="mw7 flex center bl br bt bb mb2 pa3 flex flex-column">
+				<div className="flex">
+					<Link className="pa1 mr2 black no-underline ba bw1" to={'/dashboard/browse/' + item.itemId}>View Item Details</Link>
+					<div className={btnClass + ' w4 pr3 mr3'} onClick={() => this.props.onStatusUpdate(item)}>{statusLabel}</div>
+					<div className="w-25 pt2"><strong># Bags:</strong> {item.quantity}</div>
+					<div className="b pv2 pr2">Frequency:</div>
+					<Select className="w-50 mr3 h-100"
+							options={frequencyOptions}
+							simpleValue
+							clearable={false}
+							value={item.frequency}
+							onChange={(val) => this.props.onFrequencyChange(item, val)} />
+				</div>
+				<div className="w-100 pt3"><strong>Started:</strong> {date.toLocaleDateString()}</div>
 			</div>		
 		);
 	}
