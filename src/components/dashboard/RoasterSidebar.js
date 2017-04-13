2bf0c20d5556c598d7ed0789c6c5ef0cf0876c1a
Jake Long <jakeprogrammer@gmail.com>
2017-04-13 13:10:54 -0500

    Added user icon to roaster account settings in teh sidebar

--- src/components/dashboard/RoasterSidebar.js
+++ src/components/dashboard/RoasterSidebar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import FaArchive from 'react-icons/lib/fa/archive';
 import FaRssSquare from 'react-icons/lib/fa/rss-square';
+import TiUserOutline from 'react-icons/lib/ti/user-outline';
 import SidebarSelector from './SidebarSelector';
 
 class RoasterSidebar extends Component {
@@ -12,7 +13,7 @@ class RoasterSidebar extends Component {
             <div>
                 <SidebarSelector subSelector name="Subscriptions" to={d + 'subscriptions'} location={b} icon={<FaRssSquare className="mr2 pb1"/>}/>
                 <SidebarSelector subSelector name="Inventory" to={d + 'inventory'} location={b} icon={<FaArchive className="mr2 pb1"/>}/>
-                <SidebarSelector subSelector name="Roaster Account" to={d + 'account'} location={b}/>
+                <SidebarSelector subSelector name="Roaster Account" to={d + 'account'} location={b} icon={<TiUserOutline className="mr2 pb1 f3" />}/>
             </div>
         );
     }
