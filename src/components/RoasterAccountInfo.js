1d2db819d75852a4b53bcd74fa1f9365e64737a7
Jake Long <jakeprogrammer@gmail.com>
2017-04-13 13:06:22 -0500

    Added input validation and more countries to RoasterAccountInfo

--- src/components/RoasterAccountInfo.js
+++ src/components/RoasterAccountInfo.js
@@ -52,7 +52,7 @@ class RoasterAccountInfo extends Component {
                         <input className={inputClass + ' w-100'} ref={this.props.name} placeholder="Name" defaultValue={exists ? roaster.name : ''} required/>
                     </div>
                     <div className={formRowClass}>
-                        <input className={inputClass + ' w-100'} ref={this.props.phone} placeholder="Phone Number (##########)" defaultValue={exists ? roaster.phone : ''} required/>
+                        <input className={inputClass + ' w-100'} pattern="^\d{10}$" title="Requested format is ##########" ref={this.props.phone} placeholder="Phone Number (##########)" defaultValue={exists ? roaster.phone : ''} required/>
                     </div>
                     <div className={formRowClass}>
                         <input className={inputClass + ' w-100'} ref={this.props.email} placeholder="E-mail" defaultValue={exists ? roaster.email : ''} required/>
@@ -65,7 +65,7 @@ class RoasterAccountInfo extends Component {
                     </div>
                     <div className={formRowClass}>
                         <input className={inputClass + ' w-100'} ref={this.props.city} placeholder="City or Town" defaultValue={exists ? roaster.addressCity : ''} required/>
-                        <input className={inputClass} ref={this.props.zipCode} placeholder="Zip Code" defaultValue={exists ? roaster.addressZip : ''} required/>
+                        <input className={inputClass} pattern="^\d{5}(?:[-\s]\d{4})?$" title="Please enter a valid zip code" ref={this.props.zipCode} placeholder="Zip Code" defaultValue={exists ? roaster.addressZip : ''} required/>
                     </div>
                     <div className={formRowClass}>
                         <select className={inputClass + ' w-50 pointer'} ref={this.props.state} defaultValue={exists ? roaster.addressState : ''} required>
@@ -122,7 +122,57 @@ class RoasterAccountInfo extends Component {
                             <option value="WY">Wyoming</option>
                         </select>
                         <select className={inputClass + ' w-50 pointer'} ref={this.props.country} defaultValue={exists ? roaster.addressCountry : ''} required>
-                            <option value="United States">United States</option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
                         </select>
                     </div>
                     <div className="mt3">
