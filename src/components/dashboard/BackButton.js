6c99f8d364d777a3c71ce881acc23eefe7e3bdcb
garret.meier <garret.meier@bunchball.com>
2017-04-16 22:28:18 -0500

    Add responsive sidebar functionality

--- src/components/dashboard/BackButton.js
+++ src/components/dashboard/BackButton.js
@@ -6,7 +6,7 @@ import FaArrowLeft from 'react-icons/lib/fa/arrow-left';
 class BackButton extends Component {
     render() {
         return (
-            <div>
+            <div className="dn db-l">
                 <div className={'pointer pa1 bb br bw1 black bg-white absolute'} onClick={browserHistory.goBack}>
                     <FaArrowLeft className="pv1 ph2 f1" />
                 </div>
