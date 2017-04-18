f862df71e3e251a8eda241a4ff59b813f5134adf
Jake Long <jakeprogrammer@gmail.com>
2017-04-18 01:31:14 -0500

    Styling fix

--- src/components/dashboard/roaster/subscriptions/RoasterSubscriptionDetails.js
+++ src/components/dashboard/roaster/subscriptions/RoasterSubscriptionDetails.js
@@ -11,7 +11,7 @@ class RoasterSubscriptionDetails extends Component {
 
         const linkClass = 'pointer dim br1 ba bw1 tc pa2 black no-underline';
         const rowClass = 'pa2';
-        const inputClass = 'input-reset ba b--silver pa3 mv2 db br3 mh3';
+        const inputClass = 'input-reset center ba b--silver pa3 mv2 db br3 mh3';
 
         const user = this.props.user;
         const subscription = this.props.subscription;
@@ -41,7 +41,7 @@ class RoasterSubscriptionDetails extends Component {
                     <div className={rowClass}>
                         <form className="mw7 center w-100 h-100" onSubmit={this.props.handleSubmit}>
                             <input className={inputClass + ' w-100'} ref={this.props.message} placeholder="Message" required/>
-                            <button type="submit" className="f4 w-100 link pointer dim br1 ba bw1 pv3 mb2 white bg-green">
+                            <button type="submit" className="f4 center w-100 link pointer dim br1 ba bw1 pv3 mb2 white bg-green">
                                 Contact {name}
                             </button>
                         </form>
