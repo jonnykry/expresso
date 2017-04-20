168c8de248f0fe112d2e6087f09bf4ee60ffb0ca
Jake Long <jakeprogrammer@gmail.com>
2017-04-20 17:33:41 -0500

    Removed unused import

--- src/components/dashboard/roaster/subscriptions/RoasterItem.js
+++ src/components/dashboard/roaster/subscriptions/RoasterItem.js
@@ -1,8 +1,5 @@
 import React, {Component, PropTypes} from 'react';
-import {Link} from 'react-router';
-
 import RoasterSubscription from './RoasterSubscription';
-
 import BeanItemImage from '../../browse/BeanItemImage';
 
 class RoasterItem extends Component {
