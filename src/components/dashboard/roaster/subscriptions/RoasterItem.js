828e5812f39fabf7ec987c5b6b2e07143f103bb4
Jake Long <jakeprogrammer@gmail.com>
2017-04-21 01:47:07 -0500

    Consolidated item view in roaster subscriptions. Added number of bags in stock

--- src/components/dashboard/roaster/subscriptions/RoasterItem.js
+++ src/components/dashboard/roaster/subscriptions/RoasterItem.js
@@ -9,17 +9,16 @@ class RoasterItem extends Component {
         return (
             <div className="center ba pa4 w-100 overflow-y-auto">
                 <div className="flex">
-                    <div className="pl3 flex-column w-40" style={{maxWidth: '100px'}}>
+                    <div className="pl3 flex-column w-40" style={{maxWidth: '75'}}>
                         <BeanItemImage src={this.props.bean.pictureURL} alt={this.props.bean.name}/>
                     </div>
                     <div className="pl3 flex flex-column justify-between w-60">
-                        <div>
-                            <div className="f1 b">{this.props.bean.name}</div>
-                            <div className="pv2">
-                                <span className="f2">${parseFloat(this.props.bean.consumerPrice).toFixed(2)}</span>
-                                <span className="f5 fw1 black-60 ttu">&nbsp;per {this.props.bean.ozInBag} oz bag</span>
-                            </div>
-                        </div>
+                        <div className="f1 b">{this.props.bean.name}</div>
+                    </div>
+                    <div className="pl3 flex-column w-50">
+                        <div className="f3">{this.props.bean.inStockBags} bags in stock</div>
+                        <span className="f3">${parseFloat(this.props.bean.consumerPrice).toFixed(2)}</span>
+                        <span className="f5 fw1 black-60 ttu">&nbsp;per {this.props.bean.ozInBag} oz bag</span>
                     </div>
                 </div>
                 <div className="pl3 w-100">
