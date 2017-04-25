435ff348fdd6e43bc2d3cce3383e24e33dc712c0
garret.meier <meier.garret@gmail.com>
2017-04-25 15:24:23 -0500

    Update css to make it responsive on most screens

--- src/components/dashboard/subscriptions/Subscribe.js
+++ src/components/dashboard/subscriptions/Subscribe.js
@@ -6,7 +6,7 @@ import BeanItemImage from '../browse/BeanItemImage';
 
 class Subscribe extends Component {
     render() {
-        const inputClass = 'input-reset ba b--silver pa1 mv3 db br3 mh3';
+        const inputClass = 'input-reset ba b--silver pa1 mv3 dib br3 mh3';
         const rowClass = 'mv3';
 
         const options = [
@@ -23,43 +23,46 @@ class Subscribe extends Component {
                     <div className="w-100">
                         <h1 className="tc"> Subscribe to {this.props.bean.name}</h1>
                     </div>
-                    <div className="dt-m dib">
-                        <div className="dib w-third-l w-50-m w-100 dtc-m v-top">
-                            <div className="cb center tc pa3" width="50%" height="50%">
+                    <div className="dt w-100">
+                        <div className="dib w-third-l w-50-m w-100 dtc-m v-top tc">
+                            <div className="mr2">
                                 <BeanItemImage alt={this.props.bean.name} src={this.props.bean.pictureURL}/>
                             </div>
                         </div>
-                        <div className="dib pa1 pl2 w-two-thirds-l w-50-m w-100 dtc-m v-top">
-                            <div className={rowClass}>
-                                <span className="f2">${this.props.bean.consumerPrice}</span>
-                                <span className="f5 fw1 black-60 ttu">&nbsp;per {this.props.bean.ozInBag} oz bag</span>
-                            </div>
-                            <div className={rowClass + ' w-100'}>
-                            {this.props.bean.isDecaf &&
-                                <div className="br1 bg-gold pa2 ma1 dib">Decaf</div>
-                            }
-                                <div className="br1 bg-gold pa2 ma1 dib">{this.props.bean.coffeeType}</div>
-                            </div>
-                            <div className={rowClass + ' flex'}>
-                                <p className="pv2 b">Bags (Max 10): </p><input className={inputClass + ' w2'} ref={this.props.quantityRef} onChange={this.props.handleQuantity} defaultValue={1}/>
-                            </div>
-                            <div className={rowClass + ' w-100 flex'}>
-                                <p className="pv2 b">Frequency: </p>
-                                <div className="w-50 pv3 ph2">
-                                    <Select style={{width: '100%'}}
-                                        options={options}
-                                        simpleValue
-                                        clearable={false}
-                                        value={this.props.frequency}
-                                        placeholder="Frequency"
-                                        onChange={this.props.handleFrequency}/>
+                        <div className="dib w-two-thirds-l w-50-m w-100 pl4 dtc-m v-top">
+                            <div className="mb2">
+                                <div className={rowClass}>
+                                    <span className="f2">${this.props.bean.consumerPrice}</span>
+                                    <span className="f5 fw1 black-60 ttu">&nbsp;per {this.props.bean.ozInBag} oz bag</span>
+                                </div>
+                                <div className={rowClass + ' w-100'}>
+                                {this.props.bean.isDecaf &&
+                                    <div className="br1 bg-gold pa2 ma1 dib">Decaf</div>
+                                }
+                                    <div className="br1 bg-gold pa2 ma1 dib">{this.props.bean.coffeeType}</div>
+                                </div>
+                                <div className={rowClass + ' 100'}>
+                                    <p className="pv2 b">Bags (Max 10): <input className={inputClass + ' w2'} ref={this.props.quantityRef} onChange={this.props.handleQuantity} defaultValue={1}/></p>
+                                </div>
+                                <div className={rowClass + ' flex w-100'}>
+                                    <div className="mr2 flex flex-column b"><p>Frequency:</p></div>
+                                    <div className="w-75 flex flex-column b">
+                                        <Select
+                                            options={options}
+                                            simpleValue
+                                            clearable={false}
+                                            value={this.props.frequency}
+                                            placeholder="Frequency"
+                                            onChange={this.props.handleFrequency}
+                                            />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className={rowClass + ' dib w-50-l w-100 f3'}>Price: <strong>{'$' + (this.props.bean.consumerPrice * (this.props.quantity || 1)).toFixed(2)}
                         {' ' + (this.props.frequency.charAt(0) + this.props.frequency.substr(1).toLowerCase()) || 'Monthly'}</strong></div>
-                    <div className="mv4 dib w-50 center">
+                    <div className="mv4 dib w-50-l w-100 center">
                         <div className="tc f4 w-75 center link pointer dim br1 ba bw1 pv3 mb2 white bg-green" onClick={this.props.handleSubscribe}>
                             Purchase
                         </div>
